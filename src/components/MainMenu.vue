3e71422d9380f74403338b719197f407400925d5
SundayPlayer <baptiste.vade@sundayplayer.fr>
2018-10-28 15:41:37 +0100

    add timeline vue

---
+++ src/components/MainMenu.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="main-menu">
+        <p v-if="simplify">Lecture simplifiée</p>
+        <p v-else>Lecture augmentée</p>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'MainMenu',
+    props: {
+      simplify: Boolean
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+
+</style>
