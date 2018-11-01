23ff0b3d36e260bbdabb49897f54ca8b2c593fd8
praftr <luck.pieri@gmail.com>
2018-11-01 12:22:33 +0100

    add text transition

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <MainMenu simplify></MainMenu>
     <MainMenu></MainMenu>
+    <TextComponent></TextComponent>
   </div>
 </template>
 
 <script>
 
 import MainMenu from '../components/MainMenu'
+import TextComponent from '@/components/TextComponent'
 
 export default {
   name: 'home',
   components: {
-    MainMenu
+    MainMenu,
+    TextComponent
   }
 }
 </script>
