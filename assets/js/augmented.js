3ee3f2369a7f28827330cfacdb7b51d80ac84b43
Baptiste Vadé <sundayplayer@outlook.fr>
2018-12-13 23:33:08 +0100

    misc

--- assets/js/augmented.js
+++ assets/js/augmented.js
@@ -34,4 +34,16 @@ document.addEventListener("turbolinks:load", function() {
       })
     }
   }
+
+  document.addEventListener('keydown', function(e) {
+    if (e.key === 'ArrowLeft') {
+      document.getElementById('prev').click();
+    }
+    if (e.key === 'ArrowRight') {
+      document.getElementById('next').click();
+    }
+    if (e.key === 'ArrowUp') {
+      document.getElementById('augmented-btn').click();
+    }
+  })
 })
